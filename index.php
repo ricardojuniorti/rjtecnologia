cc5aaf01f275dbce8b62749f6e5e66cf2597b62d
Ricardo Junior <ricardojuniorti@gmail.com>
2023-06-25 18:51:25 -0300

    correcoes

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
   <body>
     <!-- IE Panel-->
     <div class="ie-panel"><a href="http://windows.microsoft.com/en-US/internet-explorer/"><img src="images/ie8-panel/warning_bar_0000_us.jpg" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today."></a></div>
+    <!--
     <div class="page-loader page-loader-variant-1">
       <div>
         <img width='376' height='164' src='images/logo.png' alt=''/>
@@ -27,6 +28,7 @@
         </div>
       </div>
     </div>
+    -->
     <!-- Page-->
     <div class="page text-center" id="principal">
       <!-- Page Head-->
