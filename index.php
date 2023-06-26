ff24fb0db363b1deb0775e72eb6dfdfd74b06f36
Ricardo Junior <ricardojuniorti@gmail.com>
2023-06-26 10:53:46 -0300

    correcoes

--- index.php
+++ index.php
@@ -105,7 +105,7 @@
                 </div>
               </div>
             </div>
-            <div class="swiper-slide" data-slide-bg="https://source.unsplash.com/random/1920x750/?notebook" data-preview-bg="https://source.unsplash.com/random/1920x750/?notebook">
+            <div class="swiper-slide" data-slide-bg="https://source.unsplash.com/random/1920x750/?gamer" data-preview-bg="https://source.unsplash.com/random/1920x750/?gamer">
               <div class="swiper-caption swiper-parallax" data-speed="0.5" data-fade="true">
                 <div class="swiper-slide-caption">
                   <div class="container">
